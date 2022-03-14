74423f856ec2519d99802468a10ccdb283a0f0b2
Jandronit <84868351+Jandronit@users.noreply.github.com>
2022-03-14 14:01:35 +0100

    (lesson 230 course node) Validate Token Frontend * Implemented a type of guard, if you do not have the token validated, you are taken out of the chat.html page.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,30 @@
+const url = ( window.location.hostname.includes('localhost'))
+    ? 'http://localhost:8080/api/auth/'
+    : 'https://restserver-curso-node-8.herokuapp.com/api/auth/';
 
-const socket = io();
+let userAuth = null;
+let socket = null;
+
+const validateJWT = async() =>{
+    const token = localStorage.getItem('token') || '';
+
+    if (token.length <= 10) {
+        window.location = 'index.html';
+        throw new Error('No hay token en el servidor');
+    }
+
+    const resp = await fetch( url, {
+        headers: { 'x-token': token}
+    } );
+
+    const { userAuth: userDB, token: tokenDB } = await resp.json();
+    localStorage.setItem('token', tokenDB)
+    userAuth = userDB;
+
+}
+const main = async () => {
+    await validateJWT();
+}
+
+main();
+// const socket = io();
